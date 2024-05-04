ecca6cec765bda0a9fec7525e9705696edb3eaf1
Daniel Long <long.daniel.s@gmail.com>
2024-05-04 14:14:34 -0400

    Sorting tayloRswift maps produces better results

--- swiftascmaps/colors.py
+++ swiftascmaps/colors.py
@@ -4,39 +4,39 @@ Colors (defined as RGB values) for the Taylor Swift albums.
 
 from swiftascmaps.make_cmap import hex_to_rgb
 
-debut = hex_to_rgb([
+debut = hex_to_rgb(sorted([
     "#61b6cc",
     "#577f3f",
     "#e3e9f3",
     "#0a1605",
     "#fddac7",
     "#81a757"
-])
+]))
 
-fearless = hex_to_rgb([
+fearless = hex_to_rgb(sorted([
     "#DCBE83",
     "#88642C",
     "#EDE7D9",
     "#EFD998",
     "#49260A",
     "#EFB391"
-])
+]))
 
-speak_now = hex_to_rgb([
+speak_now = hex_to_rgb(sorted([
     "#ffffff",
     "#4b2671",
     "#5e291c",
     "#f3d8c4",
     "#f3bf73"
-])
+]))
 
-speak_now_live = hex_to_rgb([
+speak_now_live = hex_to_rgb(sorted([
     "#fce178",
     "#969696",
     "#871d20",
     "#090708",
     "#fafaf9"
-])
+]))
 
 red = [
     [40, 37, 62],
@@ -132,3 +132,16 @@ speak_now_tv = hex_to_rgb(
 nineteen_eighty_nine_tv = hex_to_rgb(
     ["#3A1F16", "#C13C25", "#D7AB8A", "#FBFAF8", "#7EA7C5", "#446D8D", "#1F2D3B"]
 )
+
+nineteen_eighty_nine_tv_sunrise_boulevard = hex_to_rgb(sorted(
+    ["#2B3849", "#6C1414", "#C8AA86", "#D0C9B9", "#888D50", "#313C40"]
+))
+
+nineteen_eighty_nine_tv_aquamarine_green = hex_to_rgb(sorted(
+["#101C18", "#C19298", "#5B96A5", "#90A5A8", "#9A241F", "#938C7C"]
+))
+
+nineteen_eighty_nine_tv_rose_garden_pink = hex_to_rgb(sorted(
+    ["#8A9DAC", "#9E948A","#DD8253", "#B4AEAF", "#B6723C", "#7F8491"]
+))
+
